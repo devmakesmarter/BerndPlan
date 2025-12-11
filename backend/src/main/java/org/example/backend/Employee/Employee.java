d86177c50bb0d42df4e7a822fb1cf7600c546e54
Ismail Erdogan <erdogan.isma@gmail.com>
2025-12-11 09:34:10 +0100

    Beginn frontend

--- backend/src/main/java/org/example/backend/Employee/Employee.java
+++ backend/src/main/java/org/example/backend/Employee/Employee.java
@@ -1,6 +1,7 @@
 package org.example.backend.Employee;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public class Employee {
@@ -13,19 +14,20 @@ public class Employee {
    private EmployeeProfession  employeeProfession;
    private LocalDate birthDate;
 
-   public Employee(String firstName, String lastName, Integer age, ExecutiveOrEmployee executiveOrEmployee, EmployeeProfession employeeProfession, LocalDate birthDate){
+   public Employee(String firstName, String lastName, ExecutiveOrEmployee executiveOrEmployee, EmployeeProfession employeeProfession, LocalDate birthDate){
+
        this.firstName = firstName;
        this.lastName = lastName;
-       this.age = age;
        this.executiveOrEmployee = executiveOrEmployee;
        this.employeeProfession = employeeProfession;
        this.birthDate = birthDate;
-
    }
 
    public Employee(){
    }
-
-
+   // Hier ist eine potenzielle Fehlerquelle ich muss noch prüfen, ob ein Minus dran ist.
+   public long getAge(){
+        return ChronoUnit.YEARS.between(LocalDate.now(), birthDate);
+   }
 
 }
--- backend/src/main/java/org/example/backend/Employee/EmployeeService.java
+++ backend/src/main/java/org/example/backend/Employee/EmployeeService.java
@@ -12,8 +12,8 @@ public class EmployeeService {
     public EmployeeService(EmployeeRepo employeeRepo){
         this.employeeRepo = employeeRepo;
 
-        Employee testEmployeeOne = new Employee("Martin", "Mensch", 31,ExecutiveOrEmployee.EMPLOYEE, EmployeeProfession.PTA, LocalDate.of(1995,2,1));
-        Employee testEmployeeTwo = new Employee("Martina", "Mensch", 28,ExecutiveOrEmployee.EXECUTIVE,EmployeeProfession.APOTHEKER, LocalDate.of(1998,2,1));
+        Employee testEmployeeOne = new Employee("Martin", "Mensch", ExecutiveOrEmployee.EMPLOYEE, EmployeeProfession.PTA, LocalDate.of(1995,2,1));
+        Employee testEmployeeTwo = new Employee("Martina", "Mensch", ExecutiveOrEmployee.EXECUTIVE,EmployeeProfession.APOTHEKER, LocalDate.of(1998,2,1));
         employeeRepo.save(testEmployeeOne);
         employeeRepo.save(testEmployeeTwo);
 
