1ddeb1529b5cfe5f90760cbeb9d7ef1b771a7667
Ismail Erdogan <erdogan.isma@gmail.com>
2025-12-12 10:44:29 +0100

    Test erweitert

--- backend/src/test/java/org/example/backend/Employee/EmployeeTest.java
+++ backend/src/test/java/org/example/backend/Employee/EmployeeTest.java
@@ -2,6 +2,8 @@ package org.example.backend.Employee;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -10,13 +12,19 @@ class EmployeeTest {
     @Test
     void testForSonar(){
         Employee testEmployee = new Employee();
+        testEmployee.setFirstName("Ismail");
+        testEmployee.setLastName("Ismail");
+        testEmployee.setEmployeeProfession(EmployeeProfession.APOTHEKER);
+        testEmployee.setExecutiveOrEmployee(ExecutiveOrEmployee.EMPLOYEE);
+        testEmployee.setBirthDate(LocalDate.of(1990,01,01));
 
-        //assertNull(testEmployee.getAge());
-        assertNull(testEmployee.getBirthDate());
-        assertNull(testEmployee.getEmployeeProfession());
-        assertNull(testEmployee.getExecutiveOrEmployee());
-        assertNull(testEmployee.getFirstName());
-        assertNull(testEmployee.getLastName());
+        assertEquals(35,testEmployee.getAge());
+        assertEquals( "Ismail",testEmployee.getFirstName());
+        assertEquals("Ismail", testEmployee.getLastName());
+        assertEquals(EmployeeProfession.APOTHEKER,testEmployee.getEmployeeProfession());
+        assertEquals(ExecutiveOrEmployee.EMPLOYEE,testEmployee.getExecutiveOrEmployee());
+        assertEquals(LocalDate.of(1990,01,01), testEmployee.getBirthDate());
+
 
     }
 
