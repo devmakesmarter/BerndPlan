e2298de45ccc25db699721e97a115fa83c1fe363
Ismail Erdogan <erdogan.isma@gmail.com>
2025-12-16 10:38:02 +0100

    Miarbeiter Karte designt

--- frontend/src/EmployeeCard.tsx
+++ frontend/src/EmployeeCard.tsx
@@ -1,18 +1,45 @@
 import type {TypeEmployee} from "./types/TypeEmployee.ts";
+import employeeImage from "./assets/pictureForEmployee.jpg";
 
 export default function EmployeeCard(props: TypeEmployee){
 
+
         return(
             <>
+                <div className="bg-gray-400 w-60 p-2 bg-gray-400 rounded-lg" >
+                    <div className="img_for_employee bg-blue-500">
+                        <img src={employeeImage}  />
+                    </div>
+                    <div className="Employee_details">
+                        <div className="flex  justify-between">
+                            <h5>{props.firstName} {props.lastName}</h5>
+                            <p>{props.age}</p>
+                        </div>
+                        <h5>{props.employeeProfession} - {props.executiveOrEmployee}</h5>
+                    </div>
+
+                </div>
+
+
+            </>
+        )
+
+
+
+
+    {/*
+            <div className="m-6 rounded-xl bg-blue-200 p-6 text-xl font-bold text-red-700 shadow-lg" >
             <div>{props.firstName}</div>
             <div>{props.lastName}</div>
             <div>{props.birthDate}</div>
             <div>{props.age}</div>
             <div>{props.employeeProfession}</div>
             <div>{props.executiveOrEmployee}</div>
+            </div>
 
-            </>
-        )
+
+            */}
+
 
 
 }
--- frontend/src/EmployeeOverview.tsx
+++ frontend/src/EmployeeOverview.tsx
@@ -2,6 +2,7 @@ import type {TypeEmployee} from "./types/TypeEmployee.ts";
 import  axios from "axios";
 import {useEffect, useState} from "react";
 import EmployeeCard from "./EmployeeCard.tsx";
+import {Form} from "react-router-dom";
 
 
 
@@ -22,7 +23,12 @@ export default function EmployeeOverview(){
 
     return(
         <>
-            {employees.map(e => <EmployeeCard firstName={e.firstName} lastName={e.lastName} age={e.age} executiveOrEmployee={e.executiveOrEmployee} employeeProfession={e.employeeProfession} birthDate={e.birthDate}/>)}
+            <div className="mainContianer bg-amber-300">
+            <form>Searchbar </form>
+            <div className="flex gap-4 ">
+                {employees.map(e => <EmployeeCard firstName={e.firstName} lastName={e.lastName} age={e.age} executiveOrEmployee={e.executiveOrEmployee} employeeProfession={e.employeeProfession} birthDate={e.birthDate}/>)}
+            </div>
+            </div>
         </>
     )
 }
